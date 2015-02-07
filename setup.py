d3d21ed5bb23d229f4b098564204fe820e7ff149
Jonty Wareing <jonty@jonty.co.uk>
2015-02-07 18:31:26 +0000

    Make the package installable using setuptools

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name='the_algorithm',
+    version='1.0',
+    description='The Pubstandards Algorithm',
+    author='Jonty Wareing',
+    author_email='jonty@jonty.co.uk',
+    url='https://github.com/jonty/the_algorithm',
+    py_modules=['the_algorithm'],
+    install_requires=['python-dateutil==2.4.0'],
+)

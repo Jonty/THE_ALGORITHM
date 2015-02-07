de19d8ef4f0bd06898a36426e1a16c8087b20955
Jonty Wareing <jonty@jonty.co.uk>
2015-02-07 18:21:07 +0000

    New algorithm, substantially more efficient
    Also includes API changes for clarity

--- the_algorithm.py
+++ the_algorithm.py
@@ -1,32 +1,59 @@
 #!/usr/bin/python
 import calendar
 import datetime
+from dateutil.relativedelta import relativedelta
 
-def get_middle_thursday(year, month):
-    startday, numdays = calendar.monthrange(year, month)
+FIRST_PUBSTANDARDS = datetime.date(year=2005, month=12, day=14)
+
+def calc_middle_thursday(year, month):
+    _, numdays = calendar.monthrange(year, month)
     middle_of_month = numdays / 2
-    middleday = 0
-    distance = 30
+    date = datetime.date(year, month, middle_of_month)
+    return middle_of_month - (date.weekday() - 3)
 
-    for day in range(1, numdays + 1):
-        weekday = calendar.weekday(year, month, day)
-        day_distance = abs(middle_of_month - day)
-        if weekday == 3 and day_distance < distance:
-            middleday = day
-            distance = day_distance
 
-    return middleday
+def gen_ps_dates(start=None):
+    if start == None:
+        start = FIRST_PUBSTANDARDS
 
-def gen_ps_dates(start):
     while True:
-        day = get_middle_thursday(start.year, start.month)
+        day = calc_middle_thursday(start.year, start.month)
         if day > start.day:
-            yield datetime.date(year=start.year, month=start.month, day=day)
+            date = datetime.datetime(
+                        year=start.year, 
+                        month=start.month, 
+                        day=day, 
+                        hour=18, 
+                        minute=0, 
+                        second=0)
+            yield date
 
-        start = datetime.date(year=start.year, month=start.month, day=1) + datetime.timedelta(days=31)
+        start = datetime.datetime(
+                    year=start.year, 
+                    month=start.month, 
+                    day=1, 
+                    hour=18, 
+                    minute=0, 
+                    second=0) + datetime.timedelta(days=31)
+
+
+def ps_offset_from_date(date):
+    return (date.year - FIRST_PUBSTANDARDS.year) * 12 + date.month - FIRST_PUBSTANDARDS.month + 1
+
+
+def ps_date_from_offset(integer):
+    offset = FIRST_PUBSTANDARDS + relativedelta(months=integer-1)
+    day = calc_middle_thursday(offset.year, offset.month)
+    date = datetime.date(year=offset.year, month=offset.month, day=day)
+    return date
+
+
+def next_ps_date():
+    now = datetime.date.today() 
+    return gen_ps_dates(start=now).next()
+
 
 if __name__ == '__main__':
-    start = datetime.date.today()
-    generator = gen_ps_dates(start)
-    for _ in range(0,10):
+    generator = gen_ps_dates(start=FIRST_PUBSTANDARDS)
+    for i in range(0,300):
         print generator.next()
